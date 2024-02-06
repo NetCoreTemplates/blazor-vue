ba06f1ff6336a35201f1d2ec9d4f842a29ba7723
Demis Bellot <demis.bellot@gmail.com>
2024-02-06 14:45:33 +0800

    Update Configure.AppHost.cs

--- MyApp/Configure.AppHost.cs
+++ MyApp/Configure.AppHost.cs
@@ -1,18 +1,8 @@
-﻿using Funq;
-using MyApp.Data;
-using MyApp.ServiceInterface;
-using ServiceStack;
-using ServiceStack.Auth;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using MyApp.ServiceModel;
-using ServiceStack.Web;
-
-[assembly: HostingStartup(typeof(MyApp.AppHost))]
+﻿[assembly: HostingStartup(typeof(MyApp.AppHost))]
 
 namespace MyApp;
 
-public class AppHost : AppHostBase, IHostingStartup
+public class AppHost() : AppHostBase("MyApp"), IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices((context, services) =>
@@ -20,10 +10,8 @@ public class AppHost : AppHostBase, IHostingStartup
             // Configure ASP.NET Core IOC Dependencies
         });
 
-    public AppHost() : base("MyApp", typeof(MyServices).Assembly) { }
-
     // Configure your AppHost with the necessary configuration and dependencies your App needs
-    public override void Configure(Container container)
+    public override void Configure()
     {
         SetConfig(new HostConfig {
         });
