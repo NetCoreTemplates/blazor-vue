ce0164d22233c8fdaf671837da5442feb6f11ae8
Demis Bellot <demis.bellot@gmail.com>
2024-03-01 03:25:04 +0800

    upgrade to the latest blog

---
+++ MyApp/Configure.Ssg.cs
@@ -0,0 +1,77 @@
+using ServiceStack.IO;
+
+[assembly: HostingStartup(typeof(MyApp.ConfigureSsg))]
+
+namespace MyApp;
+
+public class ConfigureSsg : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton<MarkdownIncludes>();
+            services.AddSingleton<MarkdownPages>();
+            services.AddSingleton<MarkdownVideos>();
+            services.AddSingleton<MarkdownBlog>();
+            services.AddSingleton<MarkdownMeta>();
+        })
+        .ConfigureAppHost(
+            appHost => appHost.Plugins.Add(new CleanUrlsFeature()),
+            afterPluginsLoaded: appHost =>
+            {
+                MarkdigConfig.Set(new MarkdigConfig
+                {
+                    ConfigurePipeline = pipeline =>
+                    {
+                        // Extend Markdig Pipeline
+                    },
+                    ConfigureContainers = config =>
+                    {
+                        config.AddBuiltInContainers();
+                        // Add Custom Block or Inline containers
+                    }
+                });
+
+                var includes = appHost.Resolve<MarkdownIncludes>();
+                var pages = appHost.Resolve<MarkdownPages>();
+                var videos = appHost.Resolve<MarkdownVideos>();
+                var blogPosts = appHost.Resolve<MarkdownBlog>();
+                var meta = appHost.Resolve<MarkdownMeta>();
+
+                meta.Features = [pages, videos, blogPosts];
+                
+                includes.LoadFrom("_includes");
+                pages.LoadFrom("_pages");
+                videos.LoadFrom("_videos");
+                blogPosts.LoadFrom("_posts");
+            },
+            afterAppHostInit: appHost =>
+            {
+                // prerender with: `$ npm run prerender` 
+                AppTasks.Register("prerender", args =>
+                {
+                    appHost.Resolve<MarkdownMeta>().RenderToAsync(
+                        metaDir: appHost.ContentRootDirectory.RealPath.CombineWith("wwwroot/meta"),
+                        baseUrl: HtmlHelpers.ToAbsoluteContentUrl("")).GetAwaiter().GetResult();
+
+                    var distDir = appHost.ContentRootDirectory.RealPath.CombineWith("dist");
+                    if (Directory.Exists(distDir))
+                        FileSystemVirtualFiles.DeleteDirectory(distDir);
+                    FileSystemVirtualFiles.CopyAll(
+                        new DirectoryInfo(appHost.ContentRootDirectory.RealPath.CombineWith("wwwroot")),
+                        new DirectoryInfo(distDir));
+                    
+                    // Render .html redirect files
+                    RazorSsg.PrerenderRedirectsAsync(appHost.ContentRootDirectory.GetFile("redirects.json"), distDir)
+                        .GetAwaiter().GetResult();
+
+                    var razorFiles = appHost.VirtualFiles.GetAllMatchingFiles("*.cshtml");
+                    RazorSsg.PrerenderAsync(appHost, razorFiles, distDir).GetAwaiter().GetResult();
+                });
+            });
+}
+
+// Add additional frontmatter info to include
+public class MarkdownFileInfo : MarkdownFileBase
+{
+}
