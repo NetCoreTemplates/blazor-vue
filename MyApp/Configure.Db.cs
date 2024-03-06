1b35305c6d3ce3f98a4e0395da7189c4d010b540
Demis Bellot <demis.bellot@gmail.com>
2024-03-06 19:27:45 +0800

    upgrade to overridable ef db mix

--- MyApp/Configure.Db.cs
+++ MyApp/Configure.Db.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using ServiceStack.Data;
 using ServiceStack.OrmLite;
+using MyApp.Data;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureDb))]
 
@@ -9,13 +11,18 @@ public class ConfigureDb : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices((context, services) => {
+            var connectionString = context.Configuration.GetConnectionString("DefaultConnection")
+                ?? "DataSource=App_Data/app.db;Cache=Shared";
+            
             services.AddSingleton<IDbConnectionFactory>(new OrmLiteConnectionFactory(
-                context.Configuration.GetConnectionString("DefaultConnection")
-                ?? ":memory:",
-                SqliteDialect.Provider));
-        })
-        .ConfigureAppHost(appHost => {
+                connectionString, SqliteDialect.Provider));
+
+            // $ dotnet ef migrations add CreateIdentitySchema
+            // $ dotnet ef database update
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString, b => b.MigrationsAssembly(nameof(MyApp))));
+            
             // Enable built-in Database Admin UI at /admin-ui/database
-            appHost.Plugins.Add(new AdminDatabaseFeature());
+            services.AddPlugin(new AdminDatabaseFeature());
         });
 }
